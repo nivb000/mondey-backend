b35c4edd745cf22e5d8cad66355620441db8a80f
Niv <bniv000@gmail.com>
2023-07-16 17:24:27 +0300

    added some features, fixed bugs

--- api/board/board.service.js
+++ api/board/board.service.js
@@ -44,6 +44,7 @@ async function add(board) {
         board.createdBy.id,
         board.createdBy.fullname,
         board.createdBy.imgUrl,
+        board.isFavorite,
         JSON.stringify(board.labels),
         JSON.stringify(board.groups),
         JSON.stringify(board.activities),
@@ -52,8 +53,8 @@ async function add(board) {
     ]
     try {
         const newBoard = await pool.query(
-            `INSERT INTO board(title, archivedat, createdat, "createdby.id", "createdby.fullname", "createdby.imgurl", labels, groups, activities, cmpsorder, members) 
-            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) RETURNING *`, values)
+            `INSERT INTO board(title, archivedat, createdat, "createdby.id", "createdby.fullname", "createdby.imgurl", "isFavorite", labels, groups, activities, cmpsorder, members) 
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11,$12) RETURNING *`, values)
         return newBoard.rows[0]
     } catch (error) {
         throw error
@@ -64,18 +65,14 @@ async function update(board, id) {
     const values = [
         board.title,
         board.archivedat,
-        board.createdat,
-        board['createdby.id'],
-        board['createdby.fullname'],
-        board['createdby.imgurl'],
-        JSON.stringify(board.labels),
+        board.isFavorite,
         JSON.stringify(board.groups),
         JSON.stringify(board.activities),
         JSON.stringify(board.cmpsorder),
         JSON.stringify(board.members)
     ]
     try {
-        await pool.query(`UPDATE board SET title = $1, archivedat = $2, createdat = $3, "createdby.id" = $4, "createdby.fullname" = $5, "createdby.imgurl" = $6, labels = $7, groups = $8, activities = $9, cmpsorder = $10, members = $11 WHERE id = ${id}`, values)
+        await pool.query(`UPDATE board SET title = $1, archivedat = $2, "isFavorite" = $3, groups = $4, activities = $5, cmpsorder = $6, members = $7 WHERE id = ${id}`, values)
         const updatedBoard = await pool.query('SELECT * FROM board WHERE id = $1', [id])
         return updatedBoard.rows[0]
     } catch (error) {
